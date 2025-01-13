7fcec66aef5379570d17e98bdd6d62a5d313d270
Justype <justype.net@outlook.com>
2025-01-13 00:06:02 +0000

    update r

--- singularity-def/ubuntu24/README.md
+++ singularity-def/ubuntu24/README.md
@@ -1,14 +1,17 @@
 - (Too large to upload) `cuda12-ubuntu24.sqf`: Ubuntu24 SquashFS with CUDA and cuDNN
 - `make-ubuntu24.sqf`: Ubuntu24 SquashFS with build tools and dependencies
-- `rstudio-ubuntu24.sqf`: Ubuntu24 SquashFS with R4.4 and RStudio
+- `r4.4.2-ubuntu24.sqf`: Ubuntu 24 SquashFS with r-base-dev 4.4.2
+- `rstudio-ubuntu24.sqf`: Ubuntu24 SquashFS with RStudio ONLY (no R)
 - `texlive-ubuntu24.sqf`: Ubuntu24 SquashFS with Pandoc and TexLive (not full, full is 2.3GB)
 - `ubuntu24.sif`: Ubuntu24 container with neovim nodejs
 
 USAGE: Use SquashFS as Overlay if needed. The latter will overwrite the former if the inner file paths are identical.
 
 ```
-singularity -o texlive-ubuntu24.sqf \
-            -o r4.4-ubuntu24.sqf \
-            -o make-ubuntu24.sqf \
-            ubuntu24.sif
+singularity shell \
+  -o texlive-ubuntu24.sqf \
+  -o rstudio-ubuntu24.sqf \
+  -o r4.4.2-ubuntu24.sqf \
+  -o make-ubuntu24.sqf \
+  ubuntu24.sif
 ```
--- singularity-def/ubuntu24/build-singularity.sh
+++ singularity-def/ubuntu24/build-singularity.sh
@@ -15,27 +15,12 @@ if [ ! -f $name.sqf ]; then
     rm -rf $name
 fi
 
-# TexLive Full
-name=texlive-ubuntu24
-if [ ! -f $name.sqf ]; then
-    singularity build --sandbox --fakeroot $name $name.def
-    cd $name && mksquashfs * ../$name.sqf && cd ..
-    sudo chown -R $USER:$USER $name # sudo required if not root
-    rm -rf $name
-fi
-
-# RStudio and R 4.4
-name=r4.4-ubuntu24
-if [ ! -f $name.sqf ]; then
-    singularity build --sandbox $name docker://rocker/rstudio:4.4
-    cd $name && mksquashfs * ../$name.sqf && cd ..
-    rm -rf $name
-fi
-
-# make
-name=make-ubuntu24
-if [ ! -f $name.sqf ]; then
-    singularity build --sandbox --fakeroot $name $name.def
-    cd $name && mksquashfs * ../$name.sqf && cd ..
-    rm -rf $name
-fi
+names=(texlive-ubuntu24 rstudio-ubuntu24 r4.4.2-ubuntu24 make-ubuntu24)
+for name in ${names[@]}; do
+    if [ ! -f $name.sqf ]; then
+        singularity build --sandbox --fakeroot $name $name.def
+        cd $name && mksquashfs * ../$name.sqf && cd ..
+        sudo chown -R $USER:$USER $name # sudo required if not root
+        rm -rf $name
+    fi
+done
